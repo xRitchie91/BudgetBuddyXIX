99f5bf713014998423d48d09ce1cebffb9c07eba
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 01:09:28 -0500

    Updated transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -7,11 +7,11 @@ const transactionSchema = new Schema(
     name: {
       type: String,
       trim: true,
-      required: "Enter a name for transaction"
+      required: "Enter transaction name"
     },
     value: {
       type: Number,
-      required: "Enter an amount"
+      required: "Enter eransaction amount"
     },
     date: {
       type: Date,
