ed65d9f427a7d946f7084c828878a2fdaacea0d7
Ritchie <74946954+xRitchie91@users.noreply.github.com>
2021-07-30 00:54:12 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// constants cause they can't change. Stubborn cause you made them that way.
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// server / mogoose
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
@@ -21,7 +23,7 @@ mongoose.connect(MONGODB_URI, {
   useFindAndModify: false
 });
 
-// routes
+// routes for our application
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
